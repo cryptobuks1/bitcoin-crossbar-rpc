95d99d0239d9ca57bd110e9ebcdc800e0bf017da
nlevo <nicolas.levo@gmail.com>
2019-04-02 10:25:01 -0400

    Add python version of bitcoinRPCbackend

--- backend/WIP/bitcoinRPCbackend.py
+++ backend/WIP/bitcoinRPCbackend.py
@@ -1,25 +1,25 @@
-import asyncio
-from os import environ
+import aiohttp, asyncio, json
 from autobahn.asyncio.wamp import ApplicationSession, ApplicationRunner
-from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
-import json
+
+async def fetch(session, url, rpcMethod='help', params=[]):
+        payload = json.dumps({"method": rpcMethod, "params": list(params), "jsonrpc": "2.0"})
+        headers = {'content-type': 'application/json'}
+        async with  session.post(url, data=payload) as response:
+            return await response.text()
 
 class Component(ApplicationSession):
-    """
-    An application component providing procedures with different kinds
-    of arguments.
-    """
 
     async def onJoin(self, details):
 
-        rpc_user, rpc_password = "name" , "password"
-        rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%(rpc_user, rpc_password))
+        async def callRPC(command, params=[]):
+            rpcUser = 'bitcoind'
+            rpcPassword = 'BTCPythonCrossbar2019'
+            rpcPort = 8332
 
-        def callRPC(command):
-            result = rpc_connection.getblockchaininfo()
-            print("backend result: ")
-            print(result)
-            return result
+            async with aiohttp.ClientSession() as session:
+                result = await fetch(session, 'http://' + rpcUser + ':' + rpcPassword + '@localhost:' + str(rpcPort), command, params)
+                print(result)
+                return result
 
         await self.register(callRPC, u'com.myapp.bitcoin')
         print("Registered methods; ready for frontend.")
@@ -27,9 +27,13 @@ class Component(ApplicationSession):
 
 if __name__ == '__main__':
     import six
-    url = environ.get("AUTOBAHN_DEMO_ROUTER", u"ws://127.0.0.1:8080/ws")
+    url = u"ws://127.0.0.1:8080/ws"
     if six.PY2 and type(url) == six.binary_type:
         url = url.decode('utf8')
     realm = u"realm1"
     runner = ApplicationRunner(url, realm)
     runner.run(Component)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+
